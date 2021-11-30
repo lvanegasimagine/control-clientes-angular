0b8f7edbb4dccdb55db3f33634c9c18be2d777aa
Luis Vanegas <lvanegas1429@gmail.com>
2021-11-30 16:01:15 -0600

    estructura realizada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceroComponent } from './components/cabecero/cabecero.component';
+import { TableroComponent } from './components/tablero/tablero.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
+import { EditarClienteComponent } from './components/editar-cliente/editar-cliente.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegistroComponent } from './auth/registro/registro.component';
+import { ConfiguracionComponent } from './components/configuracion/configuracion.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceroComponent,
+    TableroComponent,
+    ClientesComponent,
+    EditarClienteComponent,
+    LoginComponent,
+    RegistroComponent,
+    ConfiguracionComponent,
+    NotFoundComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
