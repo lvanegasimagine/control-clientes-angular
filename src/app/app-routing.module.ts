244acf8ac67ae592fc42774ea7898a11c23784da
Luis Vanegas <lvanegas1429@gmail.com>
2021-11-30 16:06:58 -0600

    Firebase instalado y rutas implementadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegistroComponent } from './auth/registro/registro.component';
+import { ConfiguracionComponent } from './components/configuracion/configuracion.component';
+import { EditarClienteComponent } from './components/editar-cliente/editar-cliente.component';
+import { TableroComponent } from './components/tablero/tablero.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TableroComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registrarse', component: RegistroComponent },
+  { path: 'configuracion', component: ConfiguracionComponent },
+  { path: 'cliente/editar/:id', component: EditarClienteComponent },
+  { path: '**', component: NotFoundComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
